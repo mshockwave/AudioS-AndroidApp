a106798da325558e79b8fbf5ed58ecd57e40fbf9
mshockwave <yihshyng223@gmail.com>
2016-03-04 10:00:11 +0800

    Converting map module to submodule

--- map/src/main/java/com/audioservice/jeffchien/audios/map/IAudioSProxy.java
+++
@@ -1,7 +0,0 @@
-package com.audioservice.jeffchien.audios.map;
-
-import com.audioservice.jeffchien.audios.rest.IAudioS;
-
-public class IAudioSProxy {
-    public static IAudioS AudioSInterface = null;
-}
